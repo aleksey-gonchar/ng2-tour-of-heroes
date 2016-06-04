8183af44b217a978c9b26597dc078e0c014f6ec1
aleksey-gonchar <gonchar.aleksey@gmail.com>
2016-06-04 11:41:19 -0300

    * readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Table of Contents
+* [Getting Started](#getting-started)
+    * [Dependencies](#dependencies)
+
+## Dependencies
+Ensure you're running the latest versions Node `v6.2.x`and NPM `3.x.x`
+
+Once you have those, you should install these globals : `webpack`, `webpack-dev-server`, `karma`, `protractor`, `typings`, `typescript`:
+
+```
+$ npm i -g webpack webpack-dev-server karma protractor typings typescript tslint
+```
+    
--- src/app/services/loopback-api/loopback-api.service.ts
+++ src/app/services/loopback-api/loopback-api.service.ts
@@ -5,35 +5,35 @@ import { Observable } from 'rxjs'
 const BASE_URL = 'http://localhost:3000/api'
 
 interface PersistedModel {
-  id?:string
+  id?: string
 }
 
 @Injectable()
 export class LoopbackApi<T extends PersistedModel> {
 
-  protected endpoint:string
-  protected httpJson:HttpJson
+  protected endpoint: string
+  protected httpJson: HttpJson
 
-  getAll():Observable<Array<T>> {
+  getAll(): Observable<Array<T>> {
     return this.httpJson.executeGet({
       url: `${BASE_URL}/${this.endpoint}`
     })
   }
 
-  create(entity:T):Observable<T> {
+  create(entity: T): Observable<T> {
     return this.httpJson.executePost({
       url: `${BASE_URL}/${this.endpoint}`,
       body: entity
     })
   }
 
-  delete(entity:T):Observable<T> {
+  delete(entity: T): Observable<T> {
     return this.httpJson.executeDelete({
       url: `${BASE_URL}/${this.endpoint}/${entity.id}`
     }).map(res => entity)
   }
 
-  update(entity:T):Observable<T> {
+  update(entity: T): Observable<T> {
     return this.httpJson.executePut({
       url: `${BASE_URL}/${this.endpoint}/${entity.id}`
     }).map(res => entity)
